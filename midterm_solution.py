a5b19d3c63921998fe8e3ac975b774c05cb65588
rdiaz21-quib <rapunzelle.diaz@fcpc.edu.ph>
2026-04-19 13:46:28 +0800

    Add user input for IGN and rank in hero roster

--- midterm_solution.py
+++ midterm_solution.py
@@ -6,3 +6,14 @@ heroes = [
     ("Chou", "Fighter")
 ]
 
+ign = input("In-game name (IGN): ")
+rank = input("Current rank: ")
+
+print("\n==========================================")
+print("   MOBILE LEGENDS -- HERO ROSTER")
+print("==========================================")
+
+for i in range(len(heroes)):
+    print(str(i + 1) + ". " + heroes[i][0] + " [" + heroes[i][1] + "]")
+
+print("==========================================")
